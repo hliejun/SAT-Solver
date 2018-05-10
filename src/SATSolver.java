1c3d0c9a6f49977a6ebc51bb32becbe873e5e72d
Chase <tiongyaocong@gmail.com>
2018-05-10 09:33:50 +0800

    modify sat solver to write csv

--- src/SATSolver.java
+++ src/SATSolver.java
@@ -1,31 +1,49 @@
 import DataStructures.Clauses;
 import DataStructures.Utilities;
-
 import Solvers.CDCL.*;
-import Solvers.Solver;
-
 import Solvers.DPLL.DPLLSolver;
 import Solvers.DPLL.RDPLLSolver;
+import Solvers.Solver;
+import org.supercsv.io.CsvMapReader;
+import org.supercsv.io.CsvMapWriter;
+import org.supercsv.io.ICsvMapReader;
+import org.supercsv.io.ICsvMapWriter;
+import org.supercsv.prefs.CsvPreference;
 
-import java.util.*;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SATSolver {
 
-    public static void main(String[] args) {
-        Solver solver = null;
+    private final boolean IS_BENCHMARKING = true;
+    private final String INVALID_ARGUMENTS_MESSAGE = "Invalid arguments expected. java SatSolver <Strategy> <Path>";
 
-        String path = "./test/testcases/unsat/5.cnf";
-//        String path = "./test/testcases/benchmark/125V_538C_sat/4.cnf"; // CDCL outperformed DPLL iterative here...
-//        String path = "./test/testcases/benchmark/250V_1065C_sat/82.cnf"; // DPLL iterative outperformed CDCL here...
+    private Strategy strategy = Strategy.AllClause_CDCL; // Default fastest strategy
+    private String strategyName = null;
+    private String path = null;
+    private Solver solver = null;
 
-//        String path = "./puzzle/einstein.cnf";
+    public void init(String[] args) {
 
-        if (args.length != 0) {
-            path = args[0];
+//        String path = "./test/testcases/unsat/5.cnf";
+////        String path = "./test/testcases/benchmark/125V_538C_sat/4.cnf"; // CDCL outperformed DPLL iterative here...
+////        String path = "./test/testcases/benchmark/250V_1065C_sat/82.cnf"; // DPLL iterative outperformed CDCL here...
+//
+////        String path = "./puzzle/einstein.cnf";
+//
+//        if (args.length != 0) {
+//            path = args[0];
+//        }
+
+        try {
+            parseArgs(args);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
         }
-        Parser parser = new Parser(path);
-        Clauses clauses = parser.getParsedClauses();
-        int literalsCount = parser.getNumOfLiterals();
+
+        parsePath();
 
         //// System.out.println(clauses.literalCount); ////
         //// System.out.println(clauses); ////
@@ -35,13 +53,35 @@ public class SATSolver {
 
 //         Strategy strategy = Strategy.Chaff_CDCL;
 //         Strategy strategy = Strategy.TwoClause_CDCL;
-        Strategy strategy = Strategy.AllClause_CDCL;
+        //Strategy strategy = Strategy.AllClause_CDCL;
         // Strategy strategy = Strategy.ERWA_CDCL;
         // Strategy strategy = Strategy.VSIDS_CDCL;
         // Strategy strategy = Strategy.Advanced_CDCL;
+    }
 
+    private double solve() {
         long startTime = System.currentTimeMillis();
 
+        if (solver != null) {
+            HashMap<String, Boolean> results = solver.solve();
+            String output = results == null ? "UNSAT" : Utilities.getOutputFromMap(results);
+            System.out.println(output);
+        } else {
+            System.out.println("Unsupported strategy.");
+        }
+
+        long endTime = System.currentTimeMillis();
+        double elapsedTime = (endTime - startTime) / 1000.0;
+        System.out.println("Execution Time: " + elapsedTime + " seconds");
+        return elapsedTime;
+    }
+
+    public static void main(String[] args) {
+        SATSolver satSolver = new SATSolver();
+        satSolver.init(args);
+    }
+
+    private void parseStrategy(Strategy strategy, Clauses clauses, int literalsCount) {
         switch(strategy) {
             case Recursive_DPLL:
                 solver = new RDPLLSolver(clauses, literalsCount);
@@ -69,20 +109,144 @@ public class SATSolver {
                 break;
             default:
                 break;
-
         }
-
-        if (solver != null) {
-            HashMap<String, Boolean> results = solver.solve();
-            String output = results == null ? "UNSAT" : Utilities.getOutputFromMap(results);
-            System.out.println(output);
-        } else {
-            System.out.println("Unsupported strategy.");
-        }
-
-        long endTime = System.currentTimeMillis();
-        double elapsedTime = (endTime - startTime) / 1000.0;
-        System.out.println("Execution Time: " + elapsedTime + " seconds");
     }
 
+    private void parseArgs(String[] args) throws IllegalArgumentException {
+        if (args.length != 2) {
+            throw new IllegalArgumentException(INVALID_ARGUMENTS_MESSAGE);
+        }
+
+        if (args[0].equals("RDPLL")) {
+            strategy = Strategy.Recursive_DPLL;
+        } else if (args[0].equals("DPLL")) {
+            strategy = Strategy.Iterative_DPLL;
+        } else if (args[0].equals("CHAFF_CDCL")) {
+            strategy = Strategy.Chaff_CDCL;
+        } else if (args[0].equals("TWOCLAUSES_CDCL")) {
+            strategy = Strategy.TwoClause_CDCL;
+        } else if (args[0].equals("ALLCLAUSES_CDCL")) {
+            strategy = Strategy.Chaff_CDCL;
+        } else if (args[0].equals("ERWA_CDCL")) {
+            strategy = Strategy.Chaff_CDCL;
+        } else if (args[0].equals("VSIDS_CDCL")) {
+            strategy = Strategy.Chaff_CDCL;
+        } else if (args[0].equals("ADVANCED_CDCL")) {
+            strategy = Strategy.Advanced_CDCL;
+        }
+
+        strategyName = args[0];
+        path = args[1];
+    }
+
+    private void parsePath() {
+        File file = new File(path);
+        if (file.exists()) {
+            try {
+                parseFile(file);
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                System.exit(0);
+            }
+        } else {
+            throw new IllegalArgumentException("Invalid path");
+        }
+    }
+
+    private void parseFile(File file) throws IOException {
+        File csvFile = new File(file.getName() + "-Template.csv");
+        if (csvFile.createNewFile() && IS_BENCHMARKING) {
+            PrintWriter writer = new PrintWriter(csvFile);
+            writer.println("S/N");
+            if (file.isDirectory()) {
+                for (int i = 1; i < file.listFiles().length + 1; i++) {
+                    writer.println(i);
+                }
+            } else {
+                writer.println(1);
+            }
+            writer.close();
+        }
+
+        String csvPath = csvFile.getPath();
+
+        if (IS_BENCHMARKING) {
+            for (Strategy strategy : Strategy.values()) {
+                this.strategy = strategy;
+                this.strategyName = strategy.name();
+                writeCSV(csvPath, file);
+                if (this.strategy == Strategy.Recursive_DPLL) {
+                    break;
+                }
+            }
+        } else {
+            writeCSV(csvPath, file);
+        }
+    }
+
+    private void writeCSV(String csvPath, File file) throws IOException {
+        ICsvMapReader mapReader = null;
+        ICsvMapWriter mapWriter = null;
+        try {
+            String outputFileName = file.getName() + ".csv";
+            File output = new File(outputFileName);
+            CsvPreference prefs = CsvPreference.STANDARD_PREFERENCE;
+            if (output.exists()) {
+                mapReader = new CsvMapReader(new FileReader(output.getPath()), prefs);
+            } else {
+                mapReader = new CsvMapReader(new FileReader(csvPath), prefs);
+            }
+            mapWriter = new CsvMapWriter(new FileWriter(outputFileName), prefs);
+
+            // header used to read the original file
+            String[] readHeader = mapReader.getHeader(true);
+            if (readHeader == null) {
+                readHeader = new String[0];
+            }
+
+            // header used to write the new file
+            // (same as 'readHeader', but with additional column)
+            final String[] writeHeader = new String[readHeader.length + 1];
+            System.arraycopy(readHeader, 0, writeHeader, 0, readHeader.length);
+            final String strategyHeader = strategyName;
+            writeHeader[writeHeader.length - 1] = strategyHeader;
+
+            mapWriter.writeHeader(writeHeader);
+
+            Map<String, String> row;
+            int filecounter = 1;
+            while ((row = mapReader.read(readHeader)) != null) {
+                if (file.isDirectory()) {
+                    String filename = filecounter + ".cnf";
+                    path = file.getAbsolutePath() + "/" + filename;
+                    row.put(strategyHeader, String.valueOf(run()));
+                    filecounter ++;
+                } else {
+                    row.put(strategyHeader, String.valueOf(run()));
+
+                }
+                // add your column with desired value
+
+                mapWriter.write(row, writeHeader);
+            }
+
+        }
+        finally {
+            if( mapReader != null ) {
+                mapReader.close();
+            }
+            if( mapWriter != null ) {
+                mapWriter.close();
+            }
+        }
+    }
+
+    private double run() throws  IOException {
+        Parser parser = new Parser(path);
+        System.out.println(path);
+        Clauses clauses = parser.getParsedClauses();
+        int literalsCount = parser.getNumOfLiterals();
+        parseStrategy(strategy, clauses, literalsCount);
+        return solve();
+    }
 }
