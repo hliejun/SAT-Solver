e486e7776efc75f02b3b8a95690847791db14f69
Chase <tiongyaocong@gmail.com>
2018-05-11 12:33:56 +0800

    scripts for instruction on how to run

---
+++ src/runFile.sh
@@ -0,0 +1,6 @@
+rm *.csv
+rm -rf previous-result
+mkdir previous-result
+mv *.txt previous-result/
+javac -cp ".:../jars/super-csv-2.3.1.jar" SATSolver.java
+java -cp ".:../jars/super-csv-2.3.1.jar" SATSolver DPLL ../test/testcases/benchmark/50V_218C_sat/1.cnf
