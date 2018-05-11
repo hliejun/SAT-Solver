10c852d61c7fd02ba450bc063a5631b41d420315
Chase <tiongyaocong@gmail.com>
2018-05-11 12:48:34 +0800

    controller script

--- src/runFile.sh
+++ src/runFile.sh
@@ -2,5 +2,11 @@ rm *.csv
 rm -rf previous-result
 mkdir previous-result
 mv *.txt previous-result/
-javac -cp ".:../jars/super-csv-2.3.1.jar" SATSolver.java
-java -cp ".:../jars/super-csv-2.3.1.jar" SATSolver DPLL ../test/testcases/benchmark/50V_218C_sat/1.cnf
+if [ -z "$1" ] && [ -z "$2" ]
+  then
+  	echo -e "./runFile.sh <Strategy> <File>"
+    echo -e "Example : ./runFile.sh DPLL ../test/testcases/benchmark/50V_218C_sat/1.cnf"
+  else
+  	javac -cp ".:../jars/super-csv-2.3.1.jar" SATSolver.java
+  	java -cp ".:../jars/super-csv-2.3.1.jar" SATSolver $1 $2
+fi
