dd5f2bf9f336b512879ae151572b00f04372699d
hliejun <liejun.hlj@gmail.com>
2018-11-11 19:48:27 +0800

    Updated files

--- result/production/CS4244-SAT-Solver/default.sh
+++
@@ -1,6 +0,0 @@
-rm *.csv
-rm -rf previous-result
-mkdir previous-result
-mv *.txt previous-result/
-javac -cp ".:../jars/super-csv-2.3.1.jar" SATSolver.java
-java -cp ".:../jars/super-csv-2.3.1.jar" SATSolver
